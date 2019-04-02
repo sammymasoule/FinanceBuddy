67d22fcc6ce7db12f4823895e7313f17e5ed5adb
jonaboy <Jonathan@agg.dk>
2019-04-02 13:08:39 +0200

    IncomeWindow oprettet uden funktionalitet

---
+++ FinanceBuddyWPF/IncomeWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinanceBuddyWPF
+{
+    /// <summary>
+    /// Interaction logic for IncomeWindow.xaml
+    /// </summary>
+    public partial class IncomeWindow : Window
+    {
+        public IncomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Income_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogOutMenuItemClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
